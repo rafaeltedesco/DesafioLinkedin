a7a58eb8bb6870d5dedbc89db9f50067530867c4
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-02-14 22:39:43 -0300

    feature: add cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import expressConfig from './src/config/expressConfig.js'
 import express from 'express'
 import { router } from './src/routes/router.routes.js'
 import { loadDb } from './src/config/dbConfig.js'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(router)
 
 app.listen(expressConfig.PORT, async ()=> {
